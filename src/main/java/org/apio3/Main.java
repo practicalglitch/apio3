89ab0dc1380452e60c9850a4335ee5ee8d624d82
practicalglitch <169734687+practicalglitch@users.noreply.github.com>
2024-07-23 16:16:03 -0400

    comment out main (this really should be a test system)

--- src/main/java/org/apio3/Main.java
+++ src/main/java/org/apio3/Main.java
@@ -1,5 +1,7 @@
 package org.apio3;
 
+import org.apio3.Types.WorkChapter;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,21 +20,25 @@ public class Main {
 		
 		//var work = ApiO3.GetWorkMetadata("55339249");
 		//writeToFile(work.oneLineOutput(), "out/workmd_" + ("55339249") + ".txt");
-		
-		/*var chapters = ApiO3.DownloadWholeWork(work.Id);
-		str = new StringBuilder();
+		/*
+		String Id = "41297265";
+		var chapters = ApiO3.DownloadWholeWork(Id);
+		StringBuilder str = new StringBuilder();
 		for(WorkChapter chapter : chapters)
 			str.append(chapter.oneLineExport());
-		writeToFile(str.toString(), "out/workchapters_" + work.Id + ".txt");
-		
-		var metadatas = ApiO3.GetChapterMetadatas(work.Id);
+		writeToFile(str.toString(), "out/workchapters_" + Id + ".txt");
+		*/
+		/*var metadatas = ApiO3.GetChapterMetadatas(work.Id);
 		str = new StringBuilder();
 		for(WorkChapter metadata : metadatas)
 			str.append(metadata.oneLineExport());
 		writeToFile(str.toString(), "out/workchmd_" + work.Id + ".txt");*/
 		
-		var chap = ApiO3.DownloadSingleChapter("144958609");
-		writeToFile(chap.oneLineExport(), "workch3_" + "144958609" + ".txt");
+		//var chap = ApiO3.DownloadSingleChapter("144958609");
+		//writeToFile(chap.oneLineExport(), "workch3_" + "144958609" + ".txt");
+
+		//if("/tags/Blue%20Archive%20(Video%20Game)/works".matches("\\/tags\\/.+\\/works"))
+		//	System.out.println("ye");
 		
 		//var fandoms = ApiO3.GetAllFandoms();
 		
@@ -48,6 +54,7 @@ public class Main {
 		var work = ApiO3.GetWorkMetadata("55700539");
 		writeToFile(work.oneLineOutput(), "out/workmd_" + work.Id + ".txt");*/
 	}
+	
 	public static void writeToFile(String data, String path){
 		try {
 			File newTextFile = new File(path);
